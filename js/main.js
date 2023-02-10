0cf8a99747dc1d1a0f008379f49b2d6b8dd74d84
Alan Kim <alankim0622@gmail.com>
2023-02-10 04:07:59 +0000

    edited css layout to match reference entry#1

--- js/main.js
+++ js/main.js
@@ -1,22 +1,21 @@
-var $photoUrl = document.querySelector('.photourl');
+var $photourl = document.querySelector('.photourl');
 var $img = document.querySelector('.placeImage');
-var $form = document.querySelector('.form');
+var $form = document.querySelector('form');
+var $title = document.querySelector('.title');
+var $note = document.querySelector('.note');
 
-$photoUrl.addEventListener('input', $photoInput);
+$photourl.addEventListener('input', $photoInput);
 function $photoInput(event) {
   $img.setAttribute('src', event.target.value);
 }
-
-var $title = document.querySelector('.title');
-var $note = document.querySelector('.note');
 
 $form.addEventListener('submit', $submit);
 function $submit(event) {
   event.preventDefault();
   var $obj = {
     title: $title.value,
-    photoUrl: $photoUrl.value,
-    notes: $note.value,
+    photourl: $photourl.value,
+    note: $note.value,
     entryId: data.nextEntryId
   };
   data.nextEntryId++;
